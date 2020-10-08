c06a07bc8c6cc13e7959b9d9017ca3a9b7e3f803
Erick Castille <ecastille924@gmail.com>
2020-10-08 00:37:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/paramble'
+require_relative '../lib/concerns/paramable'
 
 class Song
   extend Memorable
   extend Findable
-  include Paramble
+  include Paramable
   
   attr_accessor :name
   attr_reader :artist
